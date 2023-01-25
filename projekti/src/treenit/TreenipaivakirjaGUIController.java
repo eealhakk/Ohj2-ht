fb62e3309c45b69b530bc8fbfc1129a5ed0d0493
eealhakk <eeli.al.hakkinen@student.jyu.fi>
2023-01-25 20:32:44 +0200

    Alku tiedostoja

---
+++ projekti/src/treenit/TreenipaivakirjaGUIController.java
@@ -0,0 +1,10 @@
+package treenit;
+
+/**
+ * @author Eeli
+ * @version 25.1.2023
+ *
+ */
+public class TreenipaivakirjaGUIController {
+      // TODO
+}
---
+++ projekti/src/treenit/TreenipaivakirjaMain.java
@@ -0,0 +1,38 @@
+package treenit;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+
+/**
+ * @author Eeli
+ * @version 25.1.2023
+ *
+ */
+public class TreenipaivakirjaMain extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader ldr = new FXMLLoader(getClass().getResource("TreenipaivakirjaGUIView.fxml"));
+            final Pane root = ldr.load();
+            //final TreenipaivakirjaGUIController treenipaivakirjaCtrl = (TreenipaivakirjaGUIController) ldr.getController();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("treenipaivakirja.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Treenipaivakirja");
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args Ei kaytossa
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
