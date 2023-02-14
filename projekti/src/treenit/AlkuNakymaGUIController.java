79a6bf93c20a3e93921c42e1596280c1fdc12ea0
Huotari Antti Olavi <antti.huotari10@gmail.com>
2023-02-14 17:35:59 +0200

    EiToimi viestejä lisätty

--- projekti/src/treenit/AlkuNakymaGUIController.java
+++ projekti/src/treenit/AlkuNakymaGUIController.java
@@ -1,10 +1,23 @@
 package treenit;
 
+import fi.jyu.mit.fxgui.Dialogs;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 /**
  * @author antti
  * @version 9.2.2023
  *
  */
 public class AlkuNakymaGUIController {
-      // TODO
+    @FXML private TextField alkuNakAnnaVuosi;
+    
+    @FXML private void avaaAsetaVuosi() {eiToimi();}
+    
+    /**
+     * Palauttaa viestin, että kyseinen toiminto ei vielä tee mitään
+     */
+    public void eiToimi() {
+        Dialogs.showMessageDialog("Ei toimi vielä!");
+    }
 }
--- projekti/src/treenit/UusiLiikeGUIController.java
+++ projekti/src/treenit/UusiLiikeGUIController.java
@@ -1,5 +1,6 @@
 package treenit;
 
+import fi.jyu.mit.fxgui.Dialogs;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -12,22 +13,25 @@ import javafx.scene.control.TextField;
 
 public class UusiLiikeGUIController {
 
-    @FXML
-    private TextField uusiLiikeKg;
-
-    @FXML
-    private TextField uusiLiikeLiike;
-
-    @FXML
-    private TextField uusiLiikeMuut;
-
-    @FXML
-    private Button uusiLiikeSulje;
-
-    @FXML
-    private Button uusiLiikeTallenna;
-
-    @FXML
-    private TextField uusiLiikeToistot;
+    @FXML private TextField uusiLiikeKg;
+    @FXML private TextField uusiLiikeLiike;
+    @FXML private TextField uusiLiikeMuut;
+    @FXML private Button uusiLiikeSulje;
+    @FXML private Button uusiLiikeTallenna;
+    @FXML private TextField uusiLiikeToistot;
+    
+    @FXML private void avaaLiike() {eiToimi();}
+    @FXML private void avaaToistot() {eiToimi();}
+    @FXML private void avaaKg() {eiToimi();}
+    @FXML private void avaaMuut() {eiToimi();}
+    @FXML private void avaaTallenna() {eiToimi();}
+    @FXML private void avaaSulje() {eiToimi();}
+    
+    /**
+     * Näyttää vikaviestin
+     */
+    public void eiToimi() {
+        Dialogs.showMessageDialog("Ei toimi vielä!");
+    }
 
 }
