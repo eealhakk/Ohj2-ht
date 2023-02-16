953aead0d57699224f8d72958c7cb28219262f39
Huotari Antti Olavi <antti.huotari10@gmail.com>
2023-02-16 14:44:52 +0200

    varasto package ja muutoksia alkunäkymään

--- projekti/src/treenit/TreenipaivakirjaMain.java
+++ projekti/src/treenit/TreenipaivakirjaMain.java
@@ -16,12 +16,16 @@ public class TreenipaivakirjaMain extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
+            FXMLLoader ldr2 = new FXMLLoader(getClass().getResource("AlkuNakymaGUIView.fxml"));
             FXMLLoader ldr = new FXMLLoader(getClass().getResource("PaaIkkunaGUIView.fxml"));
+            final Pane root2 = ldr2.load();
             final Pane root = ldr.load();
             //final PaaIkkunaGUIController treenipaivakirjaCtrl = (PaaIkkunaGUIController) ldr.getController();
             Scene scene = new Scene(root);
+            Scene scene2 = new Scene(root2);
             scene.getStylesheets().add(getClass().getResource("treenipaivakirja.css").toExternalForm());
             primaryStage.setScene(scene);
+            primaryStage.setScene(scene2);
             primaryStage.setTitle("Treenipaivakirja");
             primaryStage.show();
         } catch(Exception e) {
