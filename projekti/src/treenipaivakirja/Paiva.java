b9affa99888350d4f59b088b03135240a52a11bf
eealhakk <eeli.al.hakkinen@student.jyu.fi>
2023-03-02 13:25:59 +0200

    Paiva ja Paivat luokkien täydennystä 13:25

--- projekti/src/treenipaivakirja/Paiva.java
+++ projekti/src/treenipaivakirja/Paiva.java
@@ -17,7 +17,10 @@ public class Paiva {
         private static int seuraavaNro    = 1;
     
     
-        public Paiva() {
+        /**
+         * Muodostaja
+         */
+        public Paiva() {//==
             // TODO Auto-generated constructor stub
         }
         
@@ -33,12 +36,17 @@ public class Paiva {
             this.vv = 2000;
         }
         
+        //Setit
+        public void setpp(int pp) {
+            this.pp = pp;
+        }
         
-        @Override
-        public String toString(){
-            String jono = String.format("%02d",pp) + "." + String.format("%02d",kk)
-                             + "." + String.format("%04d",vv);
-            return jono;
+        public void setkk(int ikk) {
+            this.kk = ikk;
+        }
+        
+        public void setvv(int ivv) {
+            this.vv = ivv;
         }
         
         public void setPv(int pvm, int kk, int vuosi) {
@@ -46,39 +54,104 @@ public class Paiva {
             this.kk = kk;
             this.vv = vuosi;
         }
+        //Setit loppuu
+        
+        @Override
+        public String toString(){
+            String jono = String.format("%02d",pp) + 
+                    "." + String.format("%02d",kk) + 
+                    "." + String.format("%04d",vv);
+            return jono;
+        }
+        
 
+
+    }//==
+    
+    //TODO: Tämän voi luultavasti siirtää muualle
+    /**
+     * palauttaa random luvun min ja max välistä. (min ja max) ei ole välillä
+     * @param min minimi
+     * @param max maksimi
+     * @return luku
+     */
+    public int ranL(int min, int max) {
+        if (min == max) return min;
+        int luku;
+        luku = (int) (Math.random() * (max - min) + min);
+        return luku;
     }
     
+    /**
+     * @param pv päivä
+     * @return päivä merkkijonona
+     */
     public String toString(Paivamaara pv){
         return pv.toString();
     }
     
+    /**
+     * @return rekisteröi uuden alkion
+     */
     public int rekisteroi() {
         tunnusNro = seuraavaNro;
         seuraavaNro++;
         return tunnusNro;
     }
     
-    public void setPv(int pvm, int kk, int vuosi) {
-        paivamaara.setPv(pvm,kk,vuosi);
+    //Setit
+    /**
+     * @param ipp asettaa päivän
+     */
+    public void setpp(int ipp) {
+        paivamaara.setpp(ipp);
     }
     
-/*
-    public String toString(Paivamaara pv) {
-        
-        int pp = Paivamaara.pp;
-        int kk = Paivamaara.;
-        int vv = Paivamaara.;
-        return Paivamaara.toString();
-        
+    /**
+     * @param ikk asettaa kuukauden
+     */
+    public void setkk(int ikk) {
+        paivamaara.setkk(ikk);
     }
-*/
-    /*
-     *         private int                 tunnusNro;
-        private final Paivamaara    paivamaara       = new Paivamaara();
-        private String              treeninTyyppi    = "";
-        private String              luontipv         = "";
-        private String              muokattuViimeksi = "";
+    
+    /**
+     * @param ivv asettaa vuoden
+     */
+    public void setvv(int ivv) {
+        paivamaara.setvv(ivv);
+    }
+    
+    /**
+     * Asettaa päivämäärän
+     * @param pvm päivä
+     * @param kk kuukausi
+     * @param vv vuosi
+     */
+    public void setPv(int pvm, int kk, int vv) {
+        paivamaara.setPv(pvm,kk,vv);
+    }
+    //Setit loppuu
+    
+
+    /**
+     * Apumetodi, jolla saadaan täytettyä testiarvot paivalle.
+     */
+    public void vastaaEsimerkkiTreeni() {
+        int ipp = ranL(1,31);
+        int ikk = ranL(1,12);
+        int ivv = ranL(2000,2023);
+        
+        paivamaara.setpp(ipp);
+        paivamaara.setpp(ikk);
+        paivamaara.setpp(ivv);
+        treeninTyyppi = "Kuntosali";
+        luontipv = "1.1.2000";
+        muokattuViimeksi = "2.2.2000";
+    }
+    
+
+    /**
+     * @param out output
      */
     public void tulosta(PrintStream out) {
         out.println(String.format("%03d", tunnusNro, 3));
@@ -88,11 +161,17 @@ public class Paiva {
     }
 
     
+    /**
+     * @param os -
+     */
     public void tulosta(OutputStream os) {
         tulosta(new PrintStream(os));
     }
 
     
+    /**
+     * @param args ei käytössä
+     */
     public static void main(String args[]) {
         Paiva eka = new Paiva();
         eka.rekisteroi();
--- projekti/src/treenipaivakirja/Paivat.java
+++ projekti/src/treenipaivakirja/Paivat.java
@@ -12,8 +12,92 @@ public class Paivat {
     private Paiva            alkiot[]      = new Paiva[MAX_PAIVIA];
 
 
+    /**
+     * Muodostaja
+     */
     public Paivat() {
-        // TODO Auto-generated constructor stub
+        // Atribuuttien oma alustus riittänee
     }
+    
+    /**
+     * @param paiva päivä luokka
+     * @throws SailoException poikkeus
+     */
+    public void lisaa(Paiva paiva) throws SailoException {
+        if (lkm >= alkiot.length) throw new SailoException("Liikaa alkioita");
+        alkiot[lkm] = paiva;
+        lkm++;
+    }
+    
+
+    /**
+     * Palauttaa viitteen i jäseneen.
+     * @param i minkä indexin jäsen halutaan
+     * @return viite jäseneen jonka idx on i
+     * @throws IndexOutOfBoundsException jos i ei ole sallitulla alueella  
+     */
+    public Paiva anna(int i) throws IndexOutOfBoundsException {
+        if (i < 0 || lkm <= i)
+            throw new IndexOutOfBoundsException("Laiton indeksi: " + i);
+        return alkiot[i];
+    }
+    
+    /**
+     * Lukee jäsenistön tiedostosta.  //TODO: Kesken
+     * @param hakemisto tiedoston hakemisto
+     * @throws SailoException jos lukeminen epäonnistuu
+     */
+    public void lueTiedostosta(String hakemisto) throws SailoException {
+        tiedostonNimi = hakemisto + "/nimet.dat";
+        throw new SailoException("Ei osata vielä lukea tiedostoa " + tiedostonNimi);
+    }
+    
+    /**
+     * Tallentaa paivat tiedostoon.  //TODO: Kesken
+     * @throws SailoException jos tallennus epäonnistuu
+     */
+    public void tallenna()throws SailoException {
+        throw new SailoException("Ei osata vielä tallentaa!" + tiedostonNimi);
+    }
+    
+    /**
+     * Palauttaa treenipaivakirjan paivien lukumäärän
+     * @return paivien lukumäärä
+     */
+    public int getlkm() {
+        return lkm;
+    }
+    
+    /**
+     * @param args ei käytössä
+     */
+    public static void main(String args[]) {
+        Paivat jasenet = new Paivat();
+
+        Paiva sali_Treeni = new Paiva(), juoksu_Treeni = new Paiva();
+        sali_Treeni.rekisteroi();
+        sali_Treeni.vastaaEsimerkkiTreeni();
+        juoksu_Treeni.rekisteroi();
+        juoksu_Treeni.vastaaEsimerkkiTreeni();
+
+        try {
+            jasenet.lisaa(sali_Treeni);
+            jasenet.lisaa(juoksu_Treeni);
+
+            System.out.println("============= Paivat testi =================");
+
+            for (int i = 0; i < jasenet.getlkm(); i++) {
+                Paiva jasen = jasenet.anna(i);
+                System.out.println("Jäsen nro: " + i);
+                jasen.tulosta(System.out);
+            }
+
+        } catch (SailoException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+
 
 }
