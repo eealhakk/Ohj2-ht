111ddf86ce318ff14688a8894c69da5e4c1e2259
Huotari Antti Olavi <antti.huotari10@gmail.com>
2023-03-02 13:52:53 +0200

    treeni ja treenit class alussa

---
+++ projekti/src/treenipaivakirja/Treeni.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package treenipaivakirja;
+
+/**
+ * @author Antti
+ * @version Mar 2, 2023
+ *
+ */
+public class Treeni {
+
+    /**
+     */
+    public static void main() {
+    // TODO Auto-generated method stub
+    
+    }
+
+}
---
+++ projekti/src/treenipaivakirja/Treenit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package treenipaivakirja;
+
+/**
+ * @author antti
+ * @version Mar 2, 2023
+ *
+ */
+public class Treenit {
+
+    /**
+     */
+    public static void main() {
+    // TODO Auto-generated method stub
+    
+    }
+
+}
