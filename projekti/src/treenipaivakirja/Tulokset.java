8edcfa6f4362017a897f233bade8f4eae364e32b
Huotari Antti Olavi <antti.huotari10@gmail.com>
2023-03-09 15:40:09 +0200

    tulos tulokset pohjat

---
+++ projekti/src/treenipaivakirja/Tulokset.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package treenipaivakirja;
+
+/**
+ * @author antti
+ * @version Mar 9, 2023
+ *
+ */
+public class Tulokset {
+
+    /**
+     * @param args eikäytö
+     */
+    public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    
+    }
+
+}
---
+++ projekti/src/treenipaivakirja/Tulos.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package treenipaivakirja;
+
+/**
+ * @author antti
+ * @version Mar 9, 2023
+ *
+ */
+public class Tulos {
+
+    /**
+     * @param args eikäytös
+     */
+    public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    
+    }
+
+}
